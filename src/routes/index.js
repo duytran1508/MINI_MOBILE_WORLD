1635c7b2be09d31693bcc28bbff620dbde2c204a
NgocDuy <ngocduyxx1508@gmail.com>
2025-02-19 15:21:59 +0700

    create initial api path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,29 @@
+const UserRouter = require("./UserRouter");
+const ProductRouter = require("./ProductRouter");
+const OrderRouter = require("./OrderRouter");
+const CartRouter = require("./CartRouter");
+const OtpRouter = require("./otpRoutes");
+const GoogleRouter = require("./GoogleRouter");
+const ReviewRouter = require("./ReviewRouter");
+const PaymentRouter = require("./PaymentRouter");
+const CategoryRouter = require("./CategoryRouter");
+const AdminRouter = require("./AdminRoutes");
+const VoucherRouter = require("./VoucherRouter");
+const AddressRouter = require("./AddressRouter");
+
 const routes = (app) => {
-    
+    app.use("/api/user", UserRouter);
+    app.use("/api/product", ProductRouter);
+    app.use("/api/order", OrderRouter);
+    app.use("/api/cart", CartRouter);
+    app.use("/api/otp", OtpRouter);
+    app.use("/", GoogleRouter);
+    app.use("/api/review", ReviewRouter);
+    app.use("/api/payments", PaymentRouter);
+    app.use("/api/category", CategoryRouter);
+    app.use("/api/check", AdminRouter);
+    app.use("/api/voucher", VoucherRouter);
+    app.use("/api/address", AddressRouter);
+  
 }
 module.exports = routes;
